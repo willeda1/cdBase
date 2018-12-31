93882f8cb9ac0299be432d4268cad22dacc06326
David Wille <david.r.wille@gmail.com>
2018-12-31 23:04:55 +0100

    appears to work now You have to (i) select category / extension and (ii) delete the core data properties file as duplicates one generated in the background

---
+++ cdBase/data/Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  cdBase
+//
+//  Created by David Wille on 31/12/2018.
+//  Copyright © 2018 David Wille. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
