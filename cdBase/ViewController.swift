3e927dfff7532b5394e1761f101deaf820f60dc5
David Wille <david.r.wille@gmail.com>
2019-01-01 11:39:02 +0100

    now reads and writes to core data

--- cdBase/ViewController.swift
+++ cdBase/ViewController.swift
@@ -7,18 +7,49 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
     private let appDelegate = UIApplication.shared.delegate as! AppDelegate
     private let context=(UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    @IBOutlet weak var display: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let person = Person(context: context)
-        person.info()
+        updateDisplay()
     }
-
-
+    
+    @IBAction func add(_ sender: Any) {
+        let person = Person(context: context)
+        person.name = "david @ \(Date())"
+        display.text="\([Person]())"
+        updateDisplay()
+    }
+    
+    func updateDisplay(){
+        
+        var entries = [Person]()
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName:"Person")
+        
+        do {
+            entries = try context.fetch(request) as! [Person]
+            
+            let noEntries = entries.count
+            var displayText = "\(noEntries) entries\n"
+            
+            for entry in entries {
+                displayText = displayText + "\n" + entry.name!
+            }
+            
+            display.text=displayText
+            
+        } catch let error as NSError {
+            display.text = "could not fetch \(error)"
+        }
+    }
+    
+    
 }
 
