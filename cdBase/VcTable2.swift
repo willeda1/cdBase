2d0ff69e08f1b3440e77d9b1d9c142288609ca49
David Wille <david.r.wille@gmail.com>
2019-01-03 10:04:29 +0100

    whoops - the double table view view has a simple VC - aim is to call two instances of a TVC

---
+++ cdBase/VcTable2.swift
@@ -0,0 +1,22 @@
+//
+//  VcTable2.swift
+//  cdBase
+//
+//  Created by David Wille on 03/01/2019.
+//  Copyright © 2019 David Wille. All rights reserved.
+//
+
+import UIKit
+
+class VcTable2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
