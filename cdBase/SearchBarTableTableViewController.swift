1af4a39c78c969671e4f8216fa26313d84afd206
David Wille <david.r.wille@gmail.com>
2019-01-04 17:41:52 +0000

    set up additional table view ready for the search bar

---
+++ cdBase/SearchBarTableTableViewController.swift
@@ -0,0 +1,137 @@
+//
+//  SearchBarTableTableViewController.swift
+//  cdBase
+//
+//  Created by David Wille on 04/01/2019.
+//  Copyright © 2019 David Wille. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SearchBarTableTableViewController: UITableViewController {
+
+    private let appDelegate=UIApplication.shared.delegate as! AppDelegate
+    private let context=(UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let request = NSFetchRequest<Person>(entityName:"Person")
+    lazy var frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+    
+    func save(){
+        appDelegate.saveContext()
+    }
+    
+    func updateFrc(){
+        save()
+        do {
+            try frc.performFetch()
+        } catch {
+            print("whoops - FRC updated failed")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("hi")
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        request.sortDescriptors=[NSSortDescriptor(key: "date", ascending: true)]
+        
+        frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        updateFrc()
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        
+        guard let sections = frc.sections else {
+            fatalError("No sections in fetchedResultsController")
+        }
+        
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "location", for: indexPath)
+        
+        // Configure the cell...
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            let person = frc.object(at: indexPath)
+            label.text=person.name
+        }
+        return cell
+    }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let person = frc.object(at: indexPath)
+            context.delete(person)
+            updateFrc()
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let fromObj=frc.object(at: fromIndexPath)
+        let toObj=frc.object(at: to)
+        let temp=fromObj.date
+        fromObj.date=toObj.date
+        toObj.date=temp
+        do {try context.save()}
+        catch {print("could not save")}
+        updateFrc()
+    }
+    
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
